950b3f61f7d70599fff4118b75f5a0d7f23623f9
MrPerruche <geninelioth@gmail.com>
2026-02-11 19:41:31 +0100

    Progrès en classe: premier succès

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ Z -> HAUTEUR
 """
 
 import math
+from dataclasses import dataclass
 from functools import lru_cache  # Memoization afin d'améliorer les performances en sauvegardant
                                  # les résultats des fonctions "déterministiques"
+from typing import Any
 
 
 DEBUG_MODE = True
@@ -47,12 +49,12 @@ def pairs(elements: tuple[int]) -> set[tuple[int, int]]:
     # Créer toutes les paires possibles
     for i in range(2 ** len(elements)): # Cette approche est acceptable. Pour 512 verres il y aura
                                         # 9 facteurs premiers -> 512 itérations de cet algorithme:
+
         x = 1
         for j, k in enumerate(elements):
-            if (i >> k) & 1 == 1:
+            if (i >> j) & 1:
                 x *= elements[j]
         y = p // x
-
         # Optimization pour éviter d'avoir à la fin des combinaisons "dupliquées" ex. (5,2) et (2,5)
         if x >= y:
             cmb.append((x, y))
@@ -60,7 +62,13 @@ def pairs(elements: tuple[int]) -> set[tuple[int, int]]:
     # Renvoyer les combinaisons en supprimant les éléments dupliqués
     return set(cmb)
 
-
+@dataclass(frozen=True, slots=True)
+class Resultat:
+    dim: tuple[float, float, float]
+    volume: float
+    grid: tuple[int, int]
+    max_glass_per_pile: int
+    variables: dict[str, Any]
 
 
 def calc_square_pyramid(
@@ -71,17 +79,17 @@ def calc_square_pyramid(
     edge_thickness: float,
     target_glass_volume: float,  # cm3 -> mL
     max_height: float,
-) -> tuple[int, int, int] | None:
+) -> list[Resultat] | None:
 
     """Calcule les dimensions de la zone occupé par les verres tel que les conditions
     target_glass_volume et glasses soit satisfait.
 
     pyramid_top / pyramide_height donne les dimensions intérieurs. Les bords du verres sont
     considérés comme à l'extérieur de ces dimensions, ce qui signifie que la largeur du verre sera
-    par exemple pyramid_top + 2 * edge_width.
+    par exemple pyramid_top + 2 * (...).
 
     Args:
-        pyramid_top (float): 
+        pyramid_top (float): TODO
 
     Returns:
         list[tuple[int, int, int]]: Dimensions X Y Z occupés par les verres de chaque arrangement
@@ -114,13 +122,14 @@ def calc_square_pyramid(
             v = (1/3)b²H - (1/3)a²H
         <=> v = (H/3)(b² - a²)
 
-    Avec cette formule, nous pouvons déterminer a, la taille de la base de la petite pyramide:
+    Avec cette formule, nous pouvons déterminer a (réel **supérieur ou égal à 0**), la taille de la
+    base de la petite pyramide:
 
             v         = (H/3)(b² - a²)
         <=> v / (H/3) = b² - a²
         <=> 3v/H - b² = -a²
         <=> a²        = (-3v / H) + b²
-        <=> a         = sqrt(b² - 3v / H)  avec a >= 0
+        <=> a         = sqrt(b² - 3v / H)
 
     Si b² - 3v / H est négatif, alors la racine sera imaginaire. Si la largeur de base est
     imaginaire, alors il est impossible de satisfaire les contraintes donc on ne peut pas émettre
@@ -133,7 +142,7 @@ def calc_square_pyramid(
         return None
     a = math.sqrt(a2)
 
-    # En théorie, a ne devrait pas être supérieur à b. Nous nous assurons néanmoins que cela est vrai
+    # En théorie, a ne devrait pas être supérieur à b. Nous vérifions quand même.
     assert a < b
 
     # S'assurer que la formule est correcte en retrouvant le volume à partir de la base
@@ -141,7 +150,7 @@ def calc_square_pyramid(
         sub_pyramid_volume = (H / 3) * (b * b + a2)
         full_pyramid_volume = (1/3) * b * b * H
         assert math.isclose(full_pyramid_volume - sub_pyramid_volume, v), \
-            f"Formule de la taille de la base intérieur incorrecte: {full_pyramid_volume - sub_pyramid_volume=}"
+            f"Formule de la taille de la base intérieur incorrecte: {full_pyramid_volume - sub_pyramid_volume=}, {a, b=}"
 
 
     # -------------------------------------------------------------------------------------------
@@ -183,7 +192,7 @@ def calc_square_pyramid(
         <=> e_h = e / cos(theta)
         <=> e_h = e / arctan(d_y / d_x)
         <=> e_h = e / arctan(c)
-    
+
     Avec e réel l'épaisseur du verre et e_h réel l'épaisseur horizontale du verre, on calcule la
     largeur extérieur de la base inférieur.
     À la base intérieur, nous ajoutons l'épaisseur horizontale du verre des deux côtés, avant de
@@ -225,7 +234,10 @@ def calc_square_pyramid(
     h = (1 - a / b) * H
     c = 2 * H / b
     e_h = e / math.atan(c)
+    theta = e / e_h  # Utilisé pour plus tard
     a_e = a + 2 * (e_h - e_h / c)
+    if a_e <= a:
+        return None
     y_c = h * (a_e - a) / (b - a)
 
     d = y_c + e
@@ -272,7 +284,7 @@ def calc_square_pyramid(
     Pour déterminer les dimensions du placard, nous aurons besoin de déterminer la taille d'un
     verre. Sa hauteur extérieur peut facilement être déterminée en ajoutant l'épaisseur du verre à
     sa base -> (h + e). Nous avons cependant besoin de calculer la largeur extérieur de la base
-    la plus grande -> base extérieur. Nous devons simplement ajouter l'épaisseur horizontalle du*
+    la plus grande -> base extérieur. Nous devons simplement ajouter l'épaisseur horizontalle du
     verre aux deux bords:
     
             b_e = b + 2e_h
@@ -280,28 +292,143 @@ def calc_square_pyramid(
     Nous déterminons d'abord le nombre de piles qui doivent être créées. Pour ca, nous arrondissons
     vers le haut la division du nombre de verres au nombre de verres que l'on peut mettre par pile.
 
-            p = ⌈n / p_m⌉
+            n_p = ⌈n / p_m⌉
 
-    Ensuite, nous déterminons toutes les combinaisons possibles de grilles de verre. Pour chaque
-    combinaison de largeur et profondeur x y entiers naturels:
+    Ici, nous pouvons avoir des piles pleines et une pile pratiquement vide. Nous déterminons le
+    nombre de verres de la pile la plus haute en prenant le plancher de la division du nombre de
+    verres demandés et le nombre de piles à créer. Ainsi, nous réduisons la hauteur du placard.
 
-    TODO
+            p = ⌈n / n_p⌉
+
+    Ensuite, nous déterminons à l'aide d'un algorithme basé sur la décomposition en facteurs
+    premiers toutes les combinaisons possibles de grilles de verre. (dans le code, nous utilisons
+    cette algorithme avec `pairs(prime_factors(p))`. Pour chaque combinaison de largeur et
+    profondeur x y entiers naturels:
+
+    Nous déterminons la hauteur et profondeur de l'armore:
+            s_x = b_e * x
+            s_y = b_e * y
+            s_y = f(p)
     """
 
+    f = lambda x: h + x*d + e
     b_e = b + 2 * e_h
-    p = math.ceil(n / p_m)
+    n_p = math.ceil(n / p_m)
+    p = math.ceil(n / n_p)
 
     # Trouver les facteurs premiers
-    glass_prime_factors = prime_factors(p)
+    glass_prime_factors = prime_factors(n_p)
     # Déterminer toutes les combinaisons possibles de ces variables
     cmb = pairs(glass_prime_factors)
 
 
-    return 0, 0, 0
+    resultats = []
+    for x, y in cmb:
+        dim = (b_e * x, b_e * y, f(p))
+        resultats.append(Resultat(
+            dim=dim,
+            volume=dim[0]*dim[1]*dim[2],
+            grid=(x, y),
+            max_glass_per_pile=p,
+            variables={
+                # Variables de taille
+                'b': b,
+                'b_e': b_e,
+                'a': a,
+                'a_e': a_e,
+                'e': e,
+                'e_h': e_h,
+                # Caractéristiques des piles
+                'n_p': n_p,
+                'p': p,
+                'H': H,
+                'h': h,
+                # Autres informations
+                'theta_pi': theta,  # Angle de la paroi du verre
+                'theta': math.degrees(theta)  # 90° -> a = b
+            }
+        ))
+        
+
+    return resultats
+
+def line_by_line_repr(l):
+    return '[\n\t' + '\n\t'.join([str(elem) for elem in l]) + ']'
 
 
+def fetch_result(results: list[Resultat] | None) -> Resultat | None:
+
+    if results is None:
+        return None
+
+    retained = results[-1]  # Avec l'implémentation actuelle, les premiers résultats sont des
+                            # grilles très longues et peu profondes et les derniers des
+                            # grilles plus équilibrées.
+
+    if (True
+        # Par ex. (et car 90° -> cube): `and retained.variables['theta'] > 60`
+    ):  # Ajouter des conditions ici.
+        return retained
+    else:
+        return None
 
 
 if __name__ == "__main__":
 
-    print((-2) ** 0.5)
+    DEBUG_MODE=False
+
+    print("Essai de la tentative 1:")
+
+    """
+            v            = (H/3)(b² - a²)
+        <=> 3v           = h(b² - a²)
+        <=> 3v / (b²-a²) = h
+    
+    Dans cette tentative, b = 7cm et a = 4cm et v = 20cL = 200mL = 200cm²
+    """
+
+
+    print(line_by_line_repr(calc_square_pyramid(
+        pyramid_top=7,
+        pyramid_height=(3*200) / (7**2 - 4**2),
+        glasses=1000,
+        edge_thickness=0.2,  # 2mm = 0.2cm
+        target_glass_volume=200,
+        max_height=40
+    )))
+    
+    print(line_by_line_repr(calc_square_pyramid(
+        pyramid_top=10.8,
+        pyramid_height=(3*202.1) / (10.8**2 - 3.6**2),
+        glasses=1000,
+        edge_thickness=0.2,  # 2mm = 0.2cm
+        target_glass_volume=200,
+        max_height=40
+    )))
+
+
+    precision = 50
+    resultats = []
+    for i in range(1_000, 30_000, precision):
+        print(f"New iteration... {i / 1_000}")
+        for j in range(1_000, 30_000, precision):
+            result: list[Resultat] | None = calc_square_pyramid(
+                pyramid_top=i/1000,
+                pyramid_height=j/1000,
+                glasses=1000,
+                edge_thickness=0.2,
+                target_glass_volume=200,
+                max_height=40
+            )
+            if result is None:
+                continue
+            taken_result: Resultat = result[-1]
+            if (
+                True
+                # and taken_result.variables['theta'] > 60
+            ):
+                resultats.append(taken_result)
+
+    resultats.sort(key=lambda x: x.volume)
+
+    print(line_by_line_repr(resultats[:10]))
